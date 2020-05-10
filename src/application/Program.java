d9a96618f26f1520cae373d5e377922b5c39185a
Yves Mattos <yvesmattos@gmail.com>
2020-05-10 19:04:27 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,6 +34,13 @@ public class Program {
 		Seller newSeller = new Seller(null, "Rob", "rob@gmail.com", new Date(), 3200.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New Id: " + newSeller.getId());
+		
+		System.out.println("\n===TEST 5: seller update ====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed! ");
+		
 	}
 }
 
